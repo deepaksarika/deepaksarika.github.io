2ae69321e50be43345d9e59399ebbdc21aabb447
deepaksarika <sarika.deepak01@gmail.com>
2024-11-24 22:36:03 -0500

    latest

--- script.js
+++
@@ -1,8 +0,0 @@
-function showProjectDetails(project) {
-    alert("More details on project: " + project);
-}
-
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Message sent successfully!");
-});
